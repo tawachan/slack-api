a5874b61cdeb1fa88baa048586bbf423a101b69a
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-09-13 23:48:58 +0900

    Add 2bots to slack

---
+++ creative-bot.rb
@@ -0,0 +1,29 @@
+require 'slack-ruby-client'
+
+token = ENV['token_parrot']
+
+Slack.configure do |config|
+  config.token = token
+end
+
+Slack::RealTime::Client.config do |config|
+  config.start_method = :rtm_start
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :message do |data|
+  if data.channel == "C726NA6M9"
+    text = data.text
+    if text.include?('くりえいてぃぶ') || text.include?('クリエイティブ')
+      client.message channel: data.channel, text: "...く、くりえいてぃぶの予感...?"
+    end
+    if text.include?(':fastparrot:')
+      client.message channel: data.channel, text: "クリエイティブが加速する(　･ิω･ิ)"
+    end
+  end
+  puts data
+end
+
+client.start!
+
---
+++ otym-bot.rb
@@ -0,0 +1,26 @@
+require 'slack-ruby-client'
+
+token = ENV['token_otym']
+
+Slack.configure do |config|
+  config.token = token
+end
+
+Slack::RealTime::Client.config do |config|
+  config.start_method = :rtm_start
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :message do |data|
+  if data.channel == "C726NA6M9"
+    text = data.text
+    if text.include?('OTYM') || text.include?('おたやま')
+      client.message channel: data.channel, text: "美しいコードは神様へのラブレターなんです。"
+    end
+  end
+  puts data
+end
+
+client.start!
+
