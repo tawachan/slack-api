9eaaaf39c113ab4d9b52d6d6763a1c34bcba9473
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-09-13 23:19:05 +0900

    Update decreative icon

--- decreative_accelerator.rb
+++ decreative_accelerator.rb
@@ -2,7 +2,7 @@ require "slack"
 
 token = ENV['token']
 puts token
-icon = 'https://media.giphy.com/media/l3q2zVr6cu95nF6O4/giphy.gif'
+icon = 'https://media.giphy.com/media/AORI3Pcli9JHG/giphy.gif'
 name = 'でぃくりえいてぃぶ担当'
 
 Slack.configure do |config|
